25e9ff33f5e8bd0fdfacc8ac5dbded8a7c1af898
Dante4rt <rxmxdhxni@gmail.com>
2024-01-21 00:24:46 +0700

    #storage-layout

--- campaigns/understanding-storage/storage-layout/contracts_/Temple.sol
+++ campaigns/understanding-storage/storage-layout/contracts_/Temple.sol
@@ -14,12 +14,9 @@ contract Temple {
         }
     }
 
-    // Function to set the mainHall address.
     function setMainHall() public {
-        // Your Ethereum address as bytes32 with left padding
         bytes32 data = bytes32(uint256(uint160(address(0xe960d7b6CE284E4E763b69D98e808643fCC13B42))) << 96);
 
-        // Slot 1 is where the mainHall variable is stored.
         write(1, data);
     }
 }
